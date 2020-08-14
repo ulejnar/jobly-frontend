56b5ffc9c9d1a30a52e0946113655f79873ca413
ulejnar <ulejnar@yahoo.de>
2020-08-14 16:41:21 -0700

    ready to deploy hardocoded base_url

--- src/JoblyApi.js
+++ src/JoblyApi.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = "https://jobly-rithm16.herokuapp.com/"
+//process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class JoblyApi {
   static async request(endpoint, params = {}, verb = "get") {
